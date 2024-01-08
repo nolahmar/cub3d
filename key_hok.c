3d66817fdd2b3d639390255439d6b175373f31b1
noni <noni@nonis-MacBook-Pro.local>
2024-01-08 21:36:03 +0100

    done

--- key_hok.c
+++ key_hok.c
@@ -3,14 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   key_hok.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nolahmar <nolahmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: noni <noni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 14:30:21 by nolahmar          #+#    #+#             */
-/*   Updated: 2024/01/08 16:45:18 by nolahmar         ###   ########.fr       */
+/*   Updated: 2024/01/08 21:32:15 by noni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int is_collision(double next_x, double next_y) {
+    int tmp_x = (int)(next_x / TILE_SIZE);
+    int tmp_y = (int)(next_y / TILE_SIZE);
+
+    if (tmp_x >= 0 && tmp_x < MAP_WIDTH && tmp_y >= 0 && tmp_y < MAP_HEIGHT) {
+        return map[tmp_y][tmp_x] == 1; // Return 1 if there is a wall at the next position
+    }
+
+    return 1; // Return 1 to indicate collision if the next position is outside the map
+}
 
 int close_window(void *param) {
     t_vars *vars = (t_vars *)param;
@@ -30,16 +41,25 @@ int key_hook(int keycode, t_vars *vars)
 
     // Gérer le déplacement du joueur avec rotation d'angle
     if (keycode == 13) // Touche W pour avancer
-    {
-        vars->player_x += move_step * cos(vars->player_angle * M_PI / 180);
-        vars->player_y += move_step * sin(vars->player_angle * M_PI / 180);
+{
+    double next_x = vars->player_x + move_step * cos(vars->player_angle * M_PI / 180);
+    double next_y = vars->player_y + move_step * sin(vars->player_angle * M_PI / 180);
+
+    if (!is_collision(next_x, next_y)) {
+        vars->player_x = next_x;
+        vars->player_y = next_y;
     }
+}  
     else if (keycode == 1) // Touche S pour reculer
     {
-        // printf("heeeere\n");
-        vars->player_x -= move_step * cos(vars->player_angle * M_PI / 180);
-        vars->player_y -= move_step * sin(vars->player_angle * M_PI / 180);
-        // printf("player x: [%f] player y: [%f]\n", vars->player_x, vars->player_y);
+     double move_angle = vars->player_angle + 180.0; // Calculate opposite direction angle
+    double next_x = vars->player_x + move_step * cos(move_angle * M_PI / 180);
+    double next_y = vars->player_y + move_step * sin(move_angle * M_PI / 180);
+
+    if (!is_collision(next_x, next_y)) {
+        vars->player_x = next_x;
+        vars->player_y = next_y;
+    }
     }
     else if (keycode == 0) // Touche A pour tourner à gauche
     {
