5b7afc06538e79bff00b74bce6b9bda20a77af01
Nouhayla Lahmar <nolahmar@e1r6p8.1337.ma>
2024-01-10 18:40:43 +0100

    cuuuub

--- key_hok.c
+++ key_hok.c
@@ -6,7 +6,7 @@
 /*   By: nolahmar <nolahmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 14:30:21 by nolahmar          #+#    #+#             */
-/*   Updated: 2024/01/09 16:43:52 by nolahmar         ###   ########.fr       */
+/*   Updated: 2024/01/10 16:39:13 by nolahmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,23 @@ int close_window(void *param) {
 
 int key_hook(int keycode, t_vars *vars)
 {
-    double move_step = 5.0;
-    double rotate_step = 2.0;
+    double move_step = 10.0;
+    double rotate_step = 10.0;
 
     if (keycode == 53) // Touche ESC pour quitter
         close_window(vars);
 
     // Gérer le déplacement du joueur avec rotation d'angle
     if (keycode == 13) // Touche W pour avancer
-{
-    double next_x = vars->player_x + move_step * cos(vars->player_angle * M_PI / 180);
-    double next_y = vars->player_y + move_step * sin(vars->player_angle * M_PI / 180);
+    {
+        double next_x = vars->player_x + move_step * cos(vars->player_angle * M_PI / 180);
+        double next_y = vars->player_y + move_step * sin(vars->player_angle * M_PI / 180);
 
-    if (!is_wall(next_x, next_y)) {
-        vars->player_x = next_x;
-        vars->player_y = next_y;
-    }
-}  
+        if (!is_wall(next_x, next_y)) {
+            vars->player_x = next_x;
+            vars->player_y = next_y;
+        }
+    }  
     else if (keycode == 1) // Touche S pour reculer
     {
      double move_angle = vars->player_angle + 180.0; // Calculate opposite direction angle
--- ray_cast.c
+++ ray_cast.c
@@ -6,7 +6,7 @@
 /*   By: nolahmar <nolahmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 14:25:07 by nolahmar          #+#    #+#             */
-/*   Updated: 2024/01/09 16:48:03 by nolahmar         ###   ########.fr       */
+/*   Updated: 2024/01/10 18:32:00 by nolahmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ void horizontal_ray_cast(t_vars *vars)
 
     hit_wall = 0;
     ray = vars->ray;
-    // printf("ray_angle: [%f]\n", ray->angle);
-    // printf("ray dir: %d\n", ray->is_down);
     ray->h_y_intersection = floor(vars->player_y / (double)TILE_SIZE) * TILE_SIZE;
     // printf("after: h_x_intersection: [%f] h_y_intersection: [%f]\n", ray->h_x_intersection, ray->h_y_intersection);
     if (ray->is_down)
@@ -46,13 +44,11 @@ void horizontal_ray_cast(t_vars *vars)
         ray->ya = -TILE_SIZE;
     ray->h_x_intersection = ((ray->h_y_intersection - vars->player_y) / (double)tan(ray->angle));
     ray->h_x_intersection += vars->player_x;
-    // printf("before: h_x_intersection: [%f] h_y_intersection: [%f]\n", ray->h_x_intersection, ray->h_y_intersection);
     if (!ray->is_down)
         ray->h_y_intersection--;
     ray->xa = TILE_SIZE / (double)tan(ray->angle);
     if ((!ray->is_right && ray->xa > 0) || (ray->is_right && ray->xa < 0))
         ray->xa *= -1;
-    // printf("y: [%f] x: [%f]\n", ray->h_y_intersection, ray->h_x_intersection);
     while (!hit_wall) {
         hit_wall = is_wall(ray->h_x_intersection, ray->h_y_intersection);
         if (hit_wall == 1)
@@ -63,7 +59,6 @@ void horizontal_ray_cast(t_vars *vars)
             ray->h_y_intersection += ray->ya;
         }
     }
-    // printf("hit_wall: [%d]\n", hit_wall);
 }
 
 void vertical_ray_cast(t_vars *vars)
@@ -100,26 +95,37 @@ void vertical_ray_cast(t_vars *vars)
     }
 }
 
-void draw_3d(t_vars *vars)
-{
-    int i;
-    double wall_height;
 
-    i = 0;
-    while (i < WINDOW_WIDTH)
+void draw_3d(t_vars *vars, int x)
+{
+    double start_wall;
+    double end_wall;
+    int y;
+    double b;
+    double distance;
+    double wall_height;
+    
+
+    b = (vars->player_angle * M_PI / 180.0) - vars->ray->angle;
+    y = 0;
+    vars->ray->distance *= cos(b);
+    distance = (WINDOW_WIDTH / 2) / tan((FOV / 2) * M_PI / 180.0);
+    wall_height = (TILE_SIZE / vars->ray->distance) * distance;
+    start_wall = (WINDOW_HEIGHT / 2) - (wall_height / 2);
+    end_wall = (WINDOW_HEIGHT / 2) + (wall_height / 2);
+    
+    for (y = 0; y < start_wall; y++)
     {
-        wall_height = (TILE_SIZE / vars->ray->distance) * 255;
-        double constant_factor = TILE_SIZE/ 255.0;
-        wall_height = constant_factor * 255;
-        int wall_start = (WINDOW_HEIGHT - wall_height) / 2;
-        wall_start += WINDOW_HEIGHT / 2;
-        int j = wall_start;
-        while (j < wall_start + wall_height)
-        {
-            mlx_pixel_put(vars->mlx, vars->win, i, j, 0xFF0000);
-            j++;
-        }
-        i++;
+        mlx_pixel_put(vars->mlx, vars->win, x, y, 0xADD8E6);
+    }
+
+    for (y = end_wall; y < WINDOW_HEIGHT; y++)
+    {
+        mlx_pixel_put(vars->mlx, vars->win, x, y, 0xFFC7C7);
+    }
+    for (y = start_wall; y <= end_wall; y++)
+    {
+        mlx_pixel_put(vars->mlx, vars->win, x, y, 0xFF0060);
     }
 }
 
@@ -152,10 +158,9 @@ void ray_cast(t_vars *vars)
             vars->ray->intersection_y = vars->ray->v_y_intersection;
             vars->ray->distance = vars->ray->v_distance;
         }
-        drawline(vars, vars->ray->intersection_x, vars->ray->intersection_y, 0xFF0000);
-        //draw_3d(vars);
+        // drawline(vars, vars->ray->intersection_x, vars->ray->intersection_y, 0xFF0000);
+        draw_3d(vars, i);
         i++;
         current_angle += FOV / (double)WINDOW_WIDTH;
-        printf("current_angle: [%f]\n", current_angle);
      }
 }
